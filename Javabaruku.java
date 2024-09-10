f312145f76ae4909de4bf5a6a9b39ff46b64d9fe
rahmanlana <rahmanlana044@gmail.com>
2024-09-10 23:14:08 +0700

    file baru

--- Javabaruku.java
+++ Javabaruku.java
@@ -1,6 +1,6 @@
 package latihan2.daspro_jobsheet2;
 
-public class MyFirstJavagithub21 {
+public class Javabaruku {
     public static void main(String[] args) {
         System.out.println("Hello from github");
     }
