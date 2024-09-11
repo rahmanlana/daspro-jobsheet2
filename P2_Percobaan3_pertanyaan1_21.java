4396f81292226e8ab114ba6e42342a8d76d4932d
rahmanlana <rahmanlana044@gmail.com>
2024-09-11 20:29:44 +0700

    pesan baru

---
+++ P2_Percobaan3_pertanyaan1_21.java
@@ -0,0 +1,25 @@
+package latihan2.daspro_jobsheet2;
+
+public class P2_Percobaan3_pertanyaan1_21 {
+    public static void main(String[] args) {
+        System.out.println("|NO|KODE MK\t|MATA KULIAH\t\t\t\t|SEMESTER|SKS|JAM|");
+        System.out.println("==========================================================================");
+        System.out.println("|1 |RTI241001\t|Pancasila\t\t\t\t|1\t |2  |2  |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|2 |RTI241002\t|Konsep Teknologi Informasi\t\t|1\t |2  |4  |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|3 |RTI241003\t|Critical Thinking dan Problem Solving  |1\t |2  |4  |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|4 |RTI241004\t|Matematika Dasar\t\t\t|1\t |3  |6  |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|5 |RTI241005\t|Rekayasa perangkat Lunak\t\t|1\t |2  |4  |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|6 |RTI241006\t|Dasar pemrograman\t\t\t|1\t |2  | 4 |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|7 |RTI241007\t|Pratikum Dasar Pemrograma\t\t|1\t |3  | 6 |");
+        System.out.println("--------------------------------------------------------------------------");
+        System.out.println("|8 |RTI241008\t|Keselamatan dan Kesehatan Kerja\t|1\t |2  | 4 |");
+        System.out.println("==========================================================================");
+
+    }
+}
