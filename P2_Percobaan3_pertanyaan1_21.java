52012e1be0b6d73473869912f0fe3759d5e0d0d6
rahmanlana <rahmanlana044@gmail.com>
2024-09-12 09:41:22 +0700

    pesan baru

--- P2_Percobaan3_pertanyaan1_21.java
+++ P2_Percobaan3_pertanyaan1_21.java
@@ -2,24 +2,16 @@ package latihan2.daspro_jobsheet2;
 
 public class P2_Percobaan3_pertanyaan1_21 {
     public static void main(String[] args) {
-        System.out.println("|NO|KODE MK    |MATA KULIAH\t\t\t\t|SEMESTER|SKS|JAM|");
-        System.out.println("==========================================================================");
-        System.out.println("|1 |RTI241001\t|Pancasila\t\t\t\t|1\t |2  |2  |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|2 |RTI241002\t|Konsep Teknologi Informasi\t\t|1\t |2  |4  |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|3 |RTI241003\t|Critical Thinking dan Problem Solving  |1\t |2  |4  |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|4 |RTI241004\t|Matematika Dasar\t\t\t|1\t |3  |6  |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|5 |RTI241005\t|Rekayasa perangkat Lunak\t\t|1\t |2  |4  |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|6 |RTI241006\t|Dasar pemrograman\t\t\t|1\t |2  | 4 |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|7 |RTI241007\t|Pratikum Dasar Pemrograma\t\t|1\t |3  | 6 |");
-        System.out.println("--------------------------------------------------------------------------");
-        System.out.println("|8 |RTI241008\t|Keselamatan dan Kesehatan Kerja\t|1\t |2  | 4 |");
-        System.out.println("==========================================================================");
-
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi Polinema --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama Saya   : Nor Rahman maulana");
+        System.out.println("NIM         : 244107020056");
+        System.out.println("Kelas       : TI - 1C");
+        System.out.println("Absen       : 21");
+        System.out.println("Prodi       : D-IV Teknik Informatika");
+        System.out.println("Alamat      : Ds Kalitidu Kec. Kalitidu Kab Bojonegoro Prov Jawa Timur");
+        System.out.println("Nomor HP    : 089601660641");
+        System.out.println("Hobi        : Sepakbola, Futsal dan volley");
     }
 }
